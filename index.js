7230cfb7937d34d372ca8485dfb3b976214309b8
Dennis Wang <66754085+hellolol2016@users.noreply.github.com>
2022-01-28 18:47:58 -0800

    started

---
+++ index.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+import chalk from 'chalk'
+import inquirer from 'inquirer'
+import gradient from 'gradient-string'
+import chalkAnimation from 'chalk-animation'
+import figlet from 'figlet'
+import { createSpinner } from 'nanospinner'
+
+let playerName;
+
+const sleep = (ms = 2000) => new Promise((r)=>setTimeout(r,ms))
+
+async function welcome(){
+  const rainbowTitle = chalkAnimation.rainbow(
+    'Who is dennis want? \n'
+  )
+  await sleep();
+  rainbowTitle.stop()
+  console.log(`
+  ${chalk.bgBlue(`HOW TO PLAY`)}
+    I am a process on your computer.
+    If you get any question wrong I will be ${chalk.bgRed('killed')}
+    So get all the questions right...
+  `)
+}
+await welcome()
+async function askName(){
+  const answers = await inquirer.prompt({
+    name:'player_name',
+    type:'input',
+    mesage:'What is your name?',
+    default(){
+      return 'Player';
+    }
+  })
+  playerName = answers.player_name
+}
+await askName()
